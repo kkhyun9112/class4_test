ece4a7c976c20688391981ef52764d858e627b84
admin <admin@admin-PC>
2017-12-12 12:41:23 +0900

    1312323123

--- test_git/src/kr/co/sist/test/TestGit.java
+++ test_git/src/kr/co/sist/test/TestGit.java
@@ -5,6 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		int i=10;
 		System.out.println("Test중 ~(O_O ~) (~ O_O )~ ~(O_O ~) (~ O_O )~"+i+"   "+i);
+		System.out.println("이거는 수정한거");
 	}
 
 }
