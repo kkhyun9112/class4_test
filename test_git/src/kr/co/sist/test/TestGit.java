b1d9dccd524d7441ada60f0c62a40260842467b0
admin <admin@admin-PC>
2017-12-12 12:14:53 +0900

    두번째 수정

--- test_git/src/kr/co/sist/test/TestGit.java
+++ test_git/src/kr/co/sist/test/TestGit.java
@@ -3,7 +3,8 @@ package kr.co.sist.test;
 public class TestGit {
 
 	public static void main(String[] args) {
-		System.out.println("Test¡ﬂ ~(O_O ~) (~ O_O )~ ~(O_O ~) (~ O_O )~");
+		int i=10;
+		System.out.println("Test¡ﬂ ~(O_O ~) (~ O_O )~ ~(O_O ~) (~ O_O )~"+i+"   "+i);
 	}
 
 }
